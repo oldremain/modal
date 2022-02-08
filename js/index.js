848bec5e400e5a822b5e175e6b9832b42565caa8
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-02-08 17:47:13 +0300

    add burger (need to fix)

--- js/index.js
+++ js/index.js
@@ -7,6 +7,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
   document.addEventListener('keydown', closeModal);
 
+  const $BURGER = document.querySelector('.menu-burger');
+  const $HEADER_NAV = document.querySelector('.header__nav');
+
+  $BURGER.addEventListener('click', openBurger);
+
+  function openBurger(e) {
+    if (e.target.parentElement == $BURGER) {
+      $HEADER_NAV.classList.add('show-header-nav');
+      document.body.style.overflow = 'hidden';
+    }
+    $BURGER.addEventListener('click', closeBurger);
+  }
+  function closeBurger(e) {
+    if (e.target.parentElement == $BURGER) {
+      $HEADER_NAV.classList.remove('show-header-nav');
+      document.body.style.overflow = '';
+    }
+  }
+
   function openModal() {
     $MODAL.classList.toggle('show');
     document.body.style.overflow = 'hidden';
